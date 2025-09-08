ebc62dee3baf853b0734a7d8b1030bbb7ea6194b
zetans <janssv.200604@gmail.com>
2025-09-08 17:54:43 -0500

    mapping v1

--- src/rl_sar/launch/gazebo.launch.py
+++ src/rl_sar/launch/gazebo.launch.py
@@ -9,7 +9,8 @@ from launch.substitutions import LaunchConfiguration, TextSubstitution, Command
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
-
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
     rname = LaunchConfiguration("rname")
@@ -43,8 +44,20 @@ def generate_launch_description():
         launch_arguments={
             # "verbose": "true",
             # "pause": "true",  # Not Available
-            "world": os.path.join(get_package_share_directory("rl_sar"), "worlds", wname + ".world"),
+            "world": '/home/zetans/IsaacLab/rl_sar/gazebo_models_worlds_collection/worlds/test_zone.world',#os.path.join(get_package_share_directory("rl_sar"), "worlds", wname + ".world"),
         }.items(),
+        
+        # PythonLaunchDescriptionSource(
+        #     PathJoinSubstitution([
+        #         FindPackageShare("ros_gz_sim"),
+        #         "launch",
+        #         "gz_sim.launch.py"
+        #     ])
+        # ),
+        
+        # launch_arguments={
+        #     "gz_args": '/home/zetans/Downloads/Warehouse/tugbot_warehouse.sdf',
+        # }.items(),
     )
 
     spawn_entity = Node(
@@ -57,7 +70,16 @@ def generate_launch_description():
         ],
         output="screen",
     )
-
+    # spawn_entity = Node(
+    #     package="ros_gz_sim",
+    #     executable="create",
+    #     arguments=[
+    #         "-name", "robot_model",
+    #         "-topic", "/robot_description",
+    #         "-z", "1.0",
+    #     ],
+    #     output="screen",
+    # )
     joint_state_broadcaster_node = Node(
         package="controller_manager",
         executable='spawner.py' if os.environ.get('ROS_DISTRO', '') == 'foxy' else 'spawner',
@@ -93,6 +115,23 @@ def generate_launch_description():
         }],
     )
 
+    rviz_config = PathJoinSubstitution([
+        FindPackageShare("rl_sar"),
+        "rviz",
+        "go2w.rviz"
+    ])
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[{"use_sim_time": True}],
+        # If you want RViz under the robot namespace, uncomment:
+        # namespace=ros_namespace,
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             "rname",
@@ -106,4 +145,5 @@ def generate_launch_description():
         # robot_joint_controller_node,  # Spawn in rl_sim.cpp
         joy_node,
         param_node,
+        rviz_node,
     ])
